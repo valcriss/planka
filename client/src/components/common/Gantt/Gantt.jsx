53cc51e2fd5b6475859d76527b1924232c810334
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-07-13 00:25:43 +0200

    Add headers to Gantt chart

--- client/src/components/common/Gantt/Gantt.jsx
+++ client/src/components/common/Gantt/Gantt.jsx
@@ -1,10 +1,13 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
 import {
   differenceInCalendarDays,
   addMonths,
   addYears,
+  addDays,
   startOfDay,
+  format,
 } from 'date-fns';
 
 import styles from './Gantt.module.scss';
@@ -13,6 +16,7 @@ const DAY_WIDTH = 24; // px width per day
 const ROW_HEIGHT = 24; // px height per task row
 
 const Gantt = React.memo(({ tasks }) => {
+  const { t } = useTranslation();
   const range = useMemo(() => {
     const today = startOfDay(new Date());
     let firstStart = null;
@@ -28,6 +32,10 @@ const Gantt = React.memo(({ tasks }) => {
     const totalDays = differenceInCalendarDays(end, start) + 1;
     return { start, end, totalDays };
   }, [tasks]);
+
+  const days = useMemo(() =>
+    Array.from({ length: range.totalDays }, (_, i) => addDays(range.start, i)),
+  [range]);
 
   const getBarStyle = (task) => {
     if (!task.startDate || !task.endDate) {
@@ -46,43 +54,64 @@ const Gantt = React.memo(({ tasks }) => {
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.leftColumn}>
-        {tasks.map((task) => (
-          <div key={task.name} className={styles.row} style={{ height: ROW_HEIGHT }}>
-            {task.name}
+      <div className={styles.headerRow}>
+        <div className={styles.leftHeader}>{t('common.epics')}</div>
+        <div className={styles.rightHeader}>
+          <div
+            className={styles.timelineHeader}
+            style={{ width: range.totalDays * DAY_WIDTH }}
+          >
+            {days.map((day) => (
+              <div
+                key={day.toISOString()}
+                className={styles.dayCell}
+                style={{ width: DAY_WIDTH }}
+              >
+                {format(day, 'd')}
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-      <div className={styles.rightColumn}>
-        <div
-          className={styles.timeline}
-          style={{ width: range.totalDays * DAY_WIDTH }}
-        >
-          {tasks.map((task) => {
-            const bar = getBarStyle(task);
-            return (
-              <div key={task.name} className={styles.row} style={{ height: ROW_HEIGHT }}>
-                {bar && (
-                  <div
-                    className={styles.bar}
-                    style={{
-                      left: bar.offset,
-                      width: bar.width,
-                      backgroundColor: task.color,
-                    }}
-                  >
+      <div className={styles.body}>
+        <div className={styles.leftColumn}>
+          {tasks.map((task) => (
+            <div key={task.name} className={styles.row} style={{ height: ROW_HEIGHT }}>
+              {task.name}
+            </div>
+          ))}
+        </div>
+        <div className={styles.rightColumn}>
+          <div
+            className={styles.timeline}
+            style={{ width: range.totalDays * DAY_WIDTH }}
+          >
+            {tasks.map((task) => {
+              const bar = getBarStyle(task);
+              return (
+                <div key={task.name} className={styles.row} style={{ height: ROW_HEIGHT }}>
+                  {bar && (
                     <div
-                      className={styles.progress}
+                      className={styles.bar}
                       style={{
-                        width: bar.progressWidth,
+                        left: bar.offset,
+                        width: bar.width,
                         backgroundColor: task.color,
                       }}
-                    />
-                  </div>
-                )}
-              </div>
-            );
-          })}
+                    >
+                      <div
+                        className={styles.progress}
+                        style={{
+                          width: bar.progressWidth,
+                          backgroundColor: task.color,
+                        }}
+                      />
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
