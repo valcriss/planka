62d7e28100919ee12ae7094bbd573ba9fdfc830d
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-07-14 15:19:14 +0200

    fix(router): guard board fetch

--- client/src/sagas/core/services/router.js
+++ client/src/sagas/core/services/router.js
@@ -211,7 +211,7 @@ export function* handleLocationChange() {
           if (currentBoard) {
             ({ id: currentBoardId } = currentBoard);
 
-            if (currentBoard.isFetching === null) {
+            if (currentBoard.isFetching === null && card.boardId) {
               try {
                 ({
                   item: board,
@@ -236,7 +236,7 @@ export function* handleLocationChange() {
                 /* empty */
               }
             }
-          } else {
+          } else if (card.boardId) {
             try {
               ({
                 item: board,
