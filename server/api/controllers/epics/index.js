88e7244960dd0441e81fe34829b545e329f2bb29
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-07-14 14:38:18 +0200

    fix(permissions): allow members to fetch epics

--- server/api/controllers/epics/index.js
+++ server/api/controllers/epics/index.js
@@ -1,28 +1,42 @@
 /* eslint-disable prettier/prettier */
 const { idInput } = require("../../../utils/inputs");
 
-const Errors = { PROJECT_NOT_FOUND: { projectNotFound: "Project not found" } };
+const Errors = { PROJECT_NOT_FOUND: { projectNotFound: 'Project not found' } };
 
 module.exports = {
   inputs: { projectId: { ...idInput, required: true } },
 
-  exits: { projectNotFound: { responseType: "notFound" } },
+  exits: { projectNotFound: { responseType: 'notFound' } },
 
   async fn({ projectId }) {
     const { currentUser } = this.req;
+
     const project = await Project.qm.getOneById(projectId);
+
     if (!project) {
       throw Errors.PROJECT_NOT_FOUND;
     }
-    const isProjectManager = await sails.helpers.users.isProjectManager(
-      currentUser.id,
-      project.id,
-    );
-    if (!isProjectManager) {
-      throw Errors.PROJECT_NOT_FOUND;
+
+    if (currentUser.role !== User.Roles.ADMIN || project.ownerProjectManagerId) {
+      const isProjectManager = await sails.helpers.users.isProjectManager(
+        currentUser.id,
+        project.id,
+      );
+
+      if (!isProjectManager) {
+        const membership = await BoardMembership.qm.getByProjectIdAndUserId(
+          project.id,
+          currentUser.id,
+        );
+
+        if (!membership) {
+          throw Errors.PROJECT_NOT_FOUND; // Forbidden
+        }
+      }
     }
 
     const epics = await Epic.qm.getByProjectId(project.id);
+
     return { items: epics };
   },
 };
