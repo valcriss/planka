6bf375f814760aed609e6b18de942e4cc4389808
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-09-23 12:49:17 +0200

    Translate French card limit strings

--- client/src/locales/fr-FR/core.js
+++ client/src/locales/fr-FR/core.js
@@ -396,9 +396,9 @@ export default {
       visualTaskManagementWithLists: 'Management visuel des tâches avec des listes.',
       withoutBaseGroup: 'Sans groupe de base',
       writeComment: 'Écrire un commentaire...',
-      cardLimit: 'Card limit',
-      cardLimitPlaceholder: '0 for no limit',
-      editCardLimit_title: 'Edit Card Limit',
+      cardLimit: 'Limite de cartes',
+      cardLimitPlaceholder: '0 pour aucune limite',
+      editCardLimit_title: 'Modifier la limite de cartes',
     },
     action: {
       activateUser: 'Activer l’utilisateur',
@@ -530,7 +530,7 @@ export default {
       unsubscribe: 'Se désabonner',
       uploadNewAvatar: 'Télécharger un nouvel avatar',
       uploadNewImage: 'Télécharger une nouvelle image',
-      editCardLimit_title: 'Edit Card Limit',
+      editCardLimit_title: 'Modifier la limite de cartes',
     },
   },
 };
