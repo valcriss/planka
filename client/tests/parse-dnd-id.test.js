7459a0b4f172268aec910ef0716009d793b80f78
Daniel Silvestre <danielsilvestre.work@gmail.com>
2025-10-09 20:24:27 +0200

    Update parse DnD id tests for structured identifiers

--- client/tests/parse-dnd-id.test.js
+++ client/tests/parse-dnd-id.test.js
@@ -1,7 +1,27 @@
 import parseDndId from '../src/utils/parse-dnd-id';
 
 describe('parseDndId', () => {
-  test('extracts id from prefixed string', () => {
-    expect(parseDndId('card:123')).toBe('123');
+  test('returns null metadata for falsy input', () => {
+    expect(parseDndId('')).toEqual({ type: null, id: null, laneKey: null });
+  });
+
+  test('extracts type and id from prefixed string', () => {
+    expect(parseDndId('card:123')).toEqual({ type: 'card', id: '123', laneKey: null });
+  });
+
+  test('extracts lane key suffix when present', () => {
+    expect(parseDndId('card:123:lane:user:42')).toEqual({
+      type: 'card',
+      id: '123',
+      laneKey: 'user:42',
+    });
+  });
+
+  test('ignores segments prior to lane descriptor', () => {
+    expect(parseDndId('list:1:card:123:lane:unassigned')).toEqual({
+      type: 'list',
+      id: '1',
+      laneKey: 'unassigned',
+    });
   });
 });
